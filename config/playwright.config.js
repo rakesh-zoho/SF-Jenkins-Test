97675a8f80a47d72cc2da041896e02a76ce065bc
rakesh-zoho <rakeshattri@zohomail.in>
2026-04-08 18:04:50 +0530

    Update playwright.config.js

--- config/playwright.config.js
+++ config/playwright.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   expect: { timeout: 15000 },
   
 reporter: [
+   ['list'],
   ['html', { outputFolder: 'reports/playwright-report', open: 'never' }],
   ['allure-playwright', { outputFolder: 'reports/allure-results' }],
   ['junit', { outputFile: 'reports/junit-results.xml' }]
